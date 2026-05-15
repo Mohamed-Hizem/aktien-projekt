82b90ca3d73b730807336bc1a261787ed0fee1b0
Mohamed Hizem <hizem.mohamed2005@gmail.com>
2026-05-15 23:02:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="Stock Market Analysis",
     page_icon="📈",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="auto"
 )
 
 # ── CUSTOM CSS ───────────────────────────────────────────────────────────────
